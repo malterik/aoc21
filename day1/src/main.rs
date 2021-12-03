90e78247e9c790c57b897cfa1060ce85aaf7ab41
Erik Schröder <malte-erik.schroeder@airbus.com>
2021-12-03 08:47:39 +0100

    Correct day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,11 @@
 use std::fs;
 
 fn main() {
+    println!("part1 solution: {}" , part1());
+    println!("part2 solution: {}" , part1());
+}
+
+fn part1() -> u32 {
     let filename = "data/input";
 
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
@@ -18,5 +23,29 @@ fn main() {
         }
         last_elem = Some(m);
     }
-    println!("counter: {}", counter)
+    counter
+}
+
+fn part2() -> u32 {
+    let filename = "data/input";
+
+    let measurements: Vec<u32> = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file")
+        .lines()
+        .map(|x| x.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>()
+        .windows(3)
+        .map( |x| x.iter().sum::<u32>())
+        .collect();
+    let mut counter = 0;
+    let mut last_elem: Option<u32> = None;
+    for m in measurements {
+        if last_elem.is_some() {
+            if m > last_elem.unwrap() {
+                counter += 1;
+            }
+        }
+        last_elem = Some(m);
+    }
+    counter
 }
